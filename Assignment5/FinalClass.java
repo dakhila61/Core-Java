cc8cf1c5a7e1fda9435aeac9d394125efa2bb76e
adharavath61 <adharavath@my.okcu.edu>
2024-01-16 10:56:31 -0600

    Created new Student file and a method getName

---
+++ Assignment5/FinalClass.java
@@ -0,0 +1,10 @@
+package Assignment5;
+
+final class FinalClass {
+    // Final variable
+    private final int finalVariable = 77;
+
+    // Final method that is package-private
+    final int getFinalVariable() {
+        return finalVariable;
+}}
