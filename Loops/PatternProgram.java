9fd3d0705b49a3acbae4f42c3a871f36b6836672
adharavath61 <adharavath@my.okcu.edu>
2024-01-17 09:46:27 -0600

    Created new Student file and a method getName

--- Loops/PatternProgram.java
+++ Loops/PatternProgram.java
@@ -1,23 +1,25 @@
 package Loops;
 
 public class PatternProgram {
-    public static void main(String[] args) {
-        int n = 5; // Number of lines
 
-        for (int i = n; i >= 1; i--) {
-            // Print '+' characters
-            for (int j = 1; j <= n - i; j++) {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int rows = 5;
+
+        // Outer loop for rows
+        for (int i = 0; i < rows; i++) {
+            // Inner loop for printing '+'
+            for (int j = 0; j < rows - i - 1; j++) {
                 System.out.print("+");
             }
 
-            // Print '-' characters
-            for (int k = 1; k <= i; k++) {
+            // Inner loop for printing '-'
+            for (int k = rows; k > rows - i - 2 ; k--) {
                 System.out.print("-");
             }
-
-            // Move to the next line after each row
             System.out.println();
-        }
-    }
+  }
+
 }
 
+}
